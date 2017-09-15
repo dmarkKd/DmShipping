364749a5ac0d8ec763fc90024d05f1ae8fe51763
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-14 18:22:47 -0700

    first commit

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
-	define('SHOPIFY_SHOP', 'kfc-food.myshopify.com');
-	define('SHOPIFY_APP_API_KEY', 'b7578934bf19aa4c0f763b16b3964a58');
-	define('SHOPIFY_APP_SHARED_SECRET', '5702c4b245785628d22a3822a2995970');
+	define('SHOPIFY_SHOP', 'logicats-demo.myshopify.com/');
+	define('SHOPIFY_APP_API_KEY', '22fad490ef397e749073e363505bf42c');
+	define('SHOPIFY_APP_SHARED_SECRET', '1ff02597c9efe537041976249e9c492b');
 	define('REDIRECT_URL', 'https://dm-shipping.herokuapp.com/oauth.php');
 
 ?>
