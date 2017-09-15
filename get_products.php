9c060614f4c20fae85a2397e3d81e9cd7888ec27
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-15 16:33:17 -0700

    product.php

--- get_products.php
+++ get_products.php
@@ -2,33 +2,31 @@
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 
-	session_start();
 
+
+	session_start();
 	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
-
 	require __DIR__.'/conf.php';
-
-	$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
+	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $_SESSION['oauth_token']);
 	try
 	{
 		# Making an API request can throw an exception
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
-	
+		print_r($products);
 	}
 	catch (shopify\ApiException $e)
 	{
 		# HTTP status code was >= 400 or response contained the key 'errors'
 		echo $e;
-		print_R($e->getRequest());
-		print_R($e->getResponse());
+		print_r($e->getRequest());
+		print_r($e->getResponse());
 	}
 	catch (shopify\CurlException $e)
 	{
 		# cURL error
 		echo $e;
-		print_R($e->getRequest());
-		print_R($e->getResponse());
+		print_r($e->getRequest());
+		print_r($e->getResponse());
 	}
-
 ?>
