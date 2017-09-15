06f59a86df802c5197360d45f4d07597d0517897
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-15 16:26:52 -0700

    product.php

--- conf.php
+++ conf.php
@@ -1,12 +1,13 @@
 <?php
-	define('SHOPIFY_SHOP', 'logicats-demo.myshopify.com');
 	//define('SHOPIFY_SHOP', 'kfc-food.myshopify.com');
-	
-	define('SHOPIFY_APP_API_KEY', '22fad490ef397e749073e363505bf42c');
 	//define('SHOPIFY_APP_API_KEY', 'cc1e250eb77547b26f75f0b662e05b3f');
-	define('SHOPIFY_APP_SHARED_SECRET', '1ff02597c9efe537041976249e9c492b');
 	//define('SHOPIFY_APP_SHARED_SECRET', 'cbd3ae9f71ea85a6b134c2b4962df6a6');
 	//define('REDIRECT_URL', 'http://192.168.1.111/dmShipping/oauth.php');
+
+
+	define('SHOPIFY_SHOP', 'logicats-demo.myshopify.com');
+	define('SHOPIFY_APP_API_KEY', '22fad490ef397e749073e363505bf42c');
+	define('SHOPIFY_APP_SHARED_SECRET', '1ff02597c9efe537041976249e9c492b');
 	define('REDIRECT_URL', 'https://dm-shipping.herokuapp.com/oauth.php');
 
 ?>
