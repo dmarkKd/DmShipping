7ecdc5c8a0e2fb492676116f4b10c5ceb04f6d08
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-15 15:55:57 -0700

    product.php

--- oauth.php
+++ oauth.php
@@ -1,24 +1,39 @@
 <?php
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
 	session_start();
+
 	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
+
 	require __DIR__.'/conf.php';
+
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
-	shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
+	// shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
+
+
 	# Step 2: http://docs.shopify.com/api/authentication/oauth#asking-for-permission
 	if (!isset($_GET['code']))
 	{
-		$permission_url = shopify\authorization_url($_GET['shop'], SHOPIFY_APP_API_KEY, array('read_content', 'write_content', 'read_themes', 'write_themes', 'read_products', 'write_products', 'read_customers', 'write_customers', 'read_orders', 'write_orders', 'read_script_tags', 'write_script_tags', 'read_fulfillments', 'write_fulfillments', 'read_shipping', 'write_shipping'));
+		$permission_url = shopify\authorization_url($_GET['shop'], SHOPIFY_APP_API_KEY, array('read_content', 'write_content', 'read_themes', 'write_themes', 'read_products', 'write_products', 'read_customers', 'write_customers', 'read_orders', 'write_orders', 'read_script_tags', 'write_script_tags', 'read_fulfillments', 'write_fulfillments', 'read_shipping', 'write_shipping'),REDIRECT_URL);
+
 		die("<script> top.location.href='$permission_url'</script>");
 	}
+
+
 	# Step 3: http://docs.shopify.com/api/authentication/oauth#confirming-installation
 	try
 	{
 		# shopify\access_token can throw an exception
 		$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
+
 		$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop'];
-		echo 'App Successfully Installed!';
+
+		// echo 'App Successfully Installed!';
+		$redirect_url = 'https://'.$_GET['shop'].'/admin/apps';
+	
+		echo "<script> top.location.href='$redirect_url'</script>";
 	}
 	catch (shopify\ApiException $e)
 	{
@@ -34,4 +49,6 @@
 		print_R($e->getRequest());
 		print_R($e->getResponse());
 	}
-?>
+
+
+?>		
