cf6e322fd4777c5bf0b698a18f142b761432435b
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-14 18:41:31 -0700

    first commit

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 	define('SHOPIFY_SHOP', 'logicats-demo.myshopify.com');
-	define('SHOPIFY_SHOP', '');
+	
 	define('SHOPIFY_APP_API_KEY', '22fad490ef397e749073e363505bf42c');
 	define('SHOPIFY_APP_SHARED_SECRET', '1ff02597c9efe537041976249e9c492b');
 	define('REDIRECT_URL', 'https://dm-shipping.herokuapp.com/oauth.php');
