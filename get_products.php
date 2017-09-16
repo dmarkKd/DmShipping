3d361b8e1abff930c49d740d6a31d7107c4c95d6
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-15 18:04:15 -0700

    product.php

--- get_products.php
+++ get_products.php
@@ -13,6 +13,14 @@
 	{
 		# Making an API request can throw an exception
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+
+		$json_url = $products;
+		$json = file_get_contents($json_url);
+		$data = json_decode($json, TRUE);
+		echo "<pre>";
+		print_r($data);
+		echo "</pre>";
+
 		print_r($products);
 	}
 	catch (shopify\ApiException $e)
