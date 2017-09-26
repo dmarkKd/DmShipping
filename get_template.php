f805d819c485802e74e992d8a2f37e1c9d52c236
Deepak Oberoi <deepak@dmarkweb.com>
2017-09-26 18:17:43 +0530

    update

---
+++ get_template.php
@@ -0,0 +1,32 @@
+<?php
+
+	session_start();
+
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+
+	require __DIR__.'/conf.php';
+
+	$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
+	print_r($shopify);
+	try
+	{
+		# Making an API request can throw an exception
+		$shop = $shopify('GET /admin/shop.json');
+	
+	}
+	catch (shopify\ApiException $e)
+	{
+		# HTTP status code was >= 400 or response contained the key 'errors'
+		echo $e;
+		print_R($e->getRequest());
+		print_R($e->getResponse());
+	}
+	catch (shopify\CurlException $e)
+	{
+		# cURL error
+		echo $e;
+		print_R($e->getRequest());
+		print_R($e->getResponse());
+	}
+?>
