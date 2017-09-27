346962c75a6623c86582204ecf43ab9ff8cc17de
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-27 16:56:54 +0530

    update

--- get_template.php
+++ get_template.php
@@ -1,35 +1,28 @@
 <?php
 
-	session_start();
+$curl = curl_init();
 
-	require __DIR__.'/vendor/autoload.php';
-	use phpish\shopify;
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://logicats-demo.myshopify.com/admin/orders.json",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Basic YTdjMzEwMmZlOWM4YmVkNTlkNzA3YmNiNWFlOGU0YTU6YmRlNWE1MDE5ZGM1NjYwMTk1NmIxNDczMmM1NmY3NmU=",
+    "cache-control: no-cache",
+    "postman-token: 170762fd-e479-1022-caaa-291ec01d257f"
+  ),
+));
 
-	require __DIR__.'/conf.php';
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
+curl_close($curl);
 
-
-	$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
-
-	print_r(SHOPIFY_SHOP);
-	try
-	{
-		# Making an API request can throw an exception
-		$shop = $shopify('GET /admin/shop.json');
-	
-	}
-	catch (shopify\ApiException $e)
-	{
-		# HTTP status code was >= 400 or response contained the key 'errors'
-		echo $e;
-		print_R($e->getRequest());
-		print_R($e->getResponse());
-	}
-	catch (shopify\CurlException $e)
-	{
-		# cURL error
-		echo $e;
-		print_R($e->getRequest());
-		print_R($e->getResponse());
-	}
-?>
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
