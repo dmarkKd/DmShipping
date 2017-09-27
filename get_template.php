8ff393a7cfe5a3a9aaf64b72703b5489b77f37d5
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-27 13:38:04 +0530

    update

--- get_template.php
+++ get_template.php
@@ -1,6 +1,5 @@
 <?php
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+
 	session_start();
 
 	require __DIR__.'/vendor/autoload.php';
@@ -10,13 +9,27 @@
 
 
 
-	// $shopify = 'https://logicats-demo.myshopify.com/admin/themes/1458896922/assets.json';
+	$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
 
-	$url = file_get_contents("https://logicats-demo.myshopify.com/admin/themes/1458896922/assets.json");
-
-	echo "<script> top.location.href='$url'</script>";
-
-
+	print_r(SHOPIFY_SHOP);
+	try
+	{
+		# Making an API request can throw an exception
+		$shop = $shopify('GET /admin/shop.json');
 	
-
+	}
+	catch (shopify\ApiException $e)
+	{
+		# HTTP status code was >= 400 or response contained the key 'errors'
+		echo $e;
+		print_R($e->getRequest());
+		print_R($e->getResponse());
+	}
+	catch (shopify\CurlException $e)
+	{
+		# cURL error
+		echo $e;
+		print_R($e->getRequest());
+		print_R($e->getResponse());
+	}
 ?>
