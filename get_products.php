c15db8dfa10cbceea89cbcdf8c78799a842c6e5d
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-27 11:41:06 +0530

    update

--- get_products.php
+++ get_products.php
@@ -8,10 +8,9 @@
 	require __DIR__.'/conf.php';
 	require __DIR__.'/func.php';
 
-	$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
-
-	$_SESSION['oauth_token'] = $oauth_token;
-    $_SESSION['shop'] = $_GET['shop'];
+	
+	print_r($_SESSION['oauth_token']); 
+   
 	
 	$shopify = shopify\client($_GET['shop'], SHOPIFY_APP_API_KEY, $oauth_token);
 	try
